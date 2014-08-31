08969d68b248982003da5c390a0cc621450702fa
edward sharp <edward@edwardsharp.net>
2014-08-30 18:03:37 -0700

    STABLE BUTTON UICOLLECTIONVIEW! YAY!

---
+++ TabSeq/TabSeq/TabSeq/ButtonCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  ButtonCollectionViewCell.swift
+//  TabSeq
+//
+//  Created by edward on 8/30/14.
+//  Copyright (c) 2014 edwardsharp. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCollectionViewCell: UICollectionViewCell {
+    
+    //@IBOutlet var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        
+//        let button   = UIButton.buttonWithType(UIButtonType.System) as UIButton
+//        button.frame = CGRectMake(100, 100, 100, 50)
+//        button.backgroundColor = UIColor.greenColor()
+//        button.setTitle("Test Button", forState: UIControlState.Normal)
+//        button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+//        
+//        self.addSubview(button)
+        //        frame.
+        
+        //        var label: UILabel = UILabel()
+        //        label.frame = CGRectMake(50, 50, 200, 21)
+        //        label.backgroundColor = UIColor.blackColor()
+        //        label.textColor = UIColor.whiteColor()
+        //        label.textAlignment = NSTextAlignment.Center
+        //        label.text = "test label"
+        //        self.addSubview(label)
+        
+        
+        
+        
+    }
+    
+    required init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+
+}
---
+++ TabSeq/TabSeq/TabSeq/FirstViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FirstViewController.swift
+//  TabSeq
+//
+//  Created by edward on 8/30/14.
+//  Copyright (c) 2014 edwardsharp. All rights reserved.
+//
+
+import UIKit
+
+var items = [String]()
+
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        items = ["", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", "","", "", "", ""]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    // #PRAGMA MARK - collectionview delegate stuffz
+    
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int
+    {
+        return items.count
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell!
+    {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as ButtonCollectionViewCell
+        //cell.imageView.image = UIImage(named:images[indexPath.row])
+        return cell
+    }
+}
+
